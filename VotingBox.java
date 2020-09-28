348d99042e19d8d451443750d0a7eba2a54d3e98
Adrian <adriancerva2021@gmail.com>
2020-09-27 20:32:15 -0500

    intruction at votingbox

--- VotingBox.java
+++ VotingBox.java
@@ -1,10 +1,12 @@
 /*
- * File: CheckerboardKarel.java
+ * File: VotingBox.java
  * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
- * worlds supplied in the starter folder.
+ *this is the first porject for week1 at merit america, 
+ *our assignment to to give command to karol robot to see 
+ *that is there a Voting Box full or empty, karel will advance box by box 
+ *if there is a ballot in the middle boxes it mean is full 
+ *but if karol see no ballot in the middle boxes it will clean the bottom box 
+ *and the upper box and will continue his way to check each ballot box
  */
 
 import stanford.karel.*;
